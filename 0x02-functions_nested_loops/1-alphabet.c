3c1c80bce09fb0d34040c57eb43e0ff0e5349ff9
Kehinde Ogundele <kehindegundel@gmail.com>
2022-09-14 00:07:50 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
+
 /**
- * print_alphabet - function to print abc
+ * print_alphabet - function to print alphabet
  *
- * Return: 0
+ * Return: always 0
+ *
  */
 void print_alphabet(void)
 {
-  char c
+  char i;
 
-    for (c = 'a'; c <= 'z'; c**)
+  for (i = 'a'; i <= 'z'; i++)
     {
-      _putchar(c);
+      _putchar(i);
     }
 _putchar('\n');
 }
