ca5f47c453b408e2309f879bd3c3ea06434cfa5f
Kehinde Ogundele <kehindegundel@gmail.com>
2022-09-14 00:31:23 -0700

    f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,11 +8,11 @@
  */
 void print_alphabet(void)
 {
-  char i;
+char i;
 
-  for (i = 'a'; i <= 'z'; i++)
-  {
+for (i = 'a'; i <= 'z'; i++)
+{
       _putchar(i);
-  }
-  _putchar('\n');
+}
+_putchar('\n');
 }
