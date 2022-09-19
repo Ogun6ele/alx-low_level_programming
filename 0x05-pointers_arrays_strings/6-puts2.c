7eb1a787b63dce1689b6a179af1957f692b1e8fd
Kehinde Ogundele <kehindegundel@gmail.com>
2022-09-19 16:53:36 -0700

    C

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,16 @@
-#include <stdio.h>                                                                                                                              
-#include "main.h"                                                                                                                               
-                                                                                                                                                
-/**                                                                                                                                             
- * puts2 - prints every other character                                                                                                         
- * @str: string                                                                                                                                 
- *                                                                                                                                              
- * Return: nothing                                                                                                                              
- */                                                                                                                                             
-                                                                                                                                                
-void puts2(char *str)                                                                                                                           
-{                                                                                                                                               
-        int i = 0;                                                                                                                              
+#include <stdio.h>
+#include "main.h"
 
+/**
+ * puts2 - prints every other character
+ * @str: string                                                                                                                                
+ *
+ * Return: nothing  
+ */                                                                                                                                             
+
+void puts2(char *str)
+{
+	int i = 0;                                                                                                                              
 	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
