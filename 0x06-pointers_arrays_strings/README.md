c7c952d29dc41671791e1dac68f0d1f1a900eb5e
Kehinde Ogundele <kehindegundel@gmail.com>
2022-09-23 01:04:59 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-A readme
+a readme
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,25 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-
-
 /**
  * void prototypes()
  * int prototypes()
  * char prototypes()
  */
-void_strcmp(char *s1, char *s2);
-char *_strncat(char *,char *,int);
-char *_strncpy(char *, char *, int);
-int _strcmp(char *, char *);
-void reverse_array(int *,int);
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+int _strcmp(char *s1, char *s2);
+char *strcat(char *dest, char *src);
+char *strncat(char *dest, char *src, int n);
+char *strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
-char *cap_string(int *str);
+char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
-void print_number(int);
-char *infinite_add(char *, char *,char *, int);
-void print_buffer(char *, int);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif
+#endif /*MAIN_H*/
