3bc435ef887d2b52cf02ef53836ac0774bc220e1
Ogun6ele <kehindegundel@gmail.com>
2023-03-02 04:37:19 -0800

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
-* main - print the string inthe put function
-*
-* Description: using the main function
-* this program prints
-* Return: 0
-*/
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints
+ * Return: 0
+ */
 int main(void)
 {
 	char c;
 	int i;
 	long li;
-	long long 11i;
+	long long lli;
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
