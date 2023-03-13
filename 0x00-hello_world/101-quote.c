0c1bad400cf7cfe4b3972d476d44517c046eafe3
Ogun6ele <kehindegundel@gmail.com>
2023-03-12 23:48:08 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - C program to print a line to the std error
+ *
  * Return 1 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Ko\
-		rpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
