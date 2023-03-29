565dc1d300301dc1a4b89e9599fa2222d5c28f72
Ogun6ele <kehindegundel@gmail.com>
2023-03-29 06:37:37 -0700

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+
 		if (i % 3 == 0 || i % 5 == 0)
 		{
 			if (i % 3 == 0)
 			{
-				 printf("Fizz");
+				printf("Fizz");
 			}
+
 			if (i % 5 == 0)
 			{
 				printf("Buzz");
@@ -26,9 +28,11 @@ int main(void)
 		{
 			 printf("%d", i);
 		}
+
 		if (i != 100)
 			printf(" ");
 	}
 	printf("\n");
+
 	return (0);
 }
