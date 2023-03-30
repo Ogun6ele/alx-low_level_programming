914ee419379cf9a1a5b0ea888a115d23b2b7f22d
Ogun6ele <kehindegundel@gmail.com>
2023-03-30 05:31:24 -0700

    Advanced task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	unsigned long int i, n;
-	
+
 
 	n = 612852475143;
 	for (i = 3; i < 782849; i = i + 2)
@@ -20,5 +20,5 @@ int main(void)
 		}
 	}
 	printf("%lu\n", n);
-	return 0;
+	return (0);
 }
